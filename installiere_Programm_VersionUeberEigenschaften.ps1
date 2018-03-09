a36a1fa83b97468afc6212f1863254d7ac8d6bc7
Mike <37189890+F1D3LL1O@users.noreply.github.com>
2018-03-09 15:25:22 +0100

    Create installiere_Programm_VersionUeberEigenschaften.ps1

---
+++ installiere_Programm_VersionUeberEigenschaften.ps1
@@ -0,0 +1,73 @@
+".........................####...##..##..######..##......##......######..######...####...##..##........................"
+"........................##......##..##..##......##......##......##........##....##......##..##........................"
+".........................####...######..####....##......##......####......##.....####...######........................"
+"............................##..##..##..##......##......##......##........##........##..##..##........................"
+".........................####...##..##..######..######..######..##......######...####...##..##........................"
+"......................................................................................................................"
+".####....####...#####...######..#####...######...####...........######...####...#####...........##..##...####...##..##"
+"##......##..##..##..##....##....##..##....##....##..............##......##..##..##..##...........####...##..##..##..##"
+".####...##......#####.....##....#####.....##.....####...........####....##..##..#####.............##....##..##..##..##"
+"....##..##..##..##..##....##....##........##........##..........##......##..##..##..##............##....##..##..##..##"
+".####....####...##..##..######..##........##.....####...........##.......####...##..##............##.....####....####."
+"......................................................................................................................"
+
+#Setze Programmnamen
+$progName = "Supertolles Programm"
+#Setze aktuell gewünscherte Version auf:
+$desiredProgVer = "17.0"
+#Setze Name der zu überprüfenden Datei
+$dateiname = "\Tolle Firma\Supertolles Programm\Programmdatei_mit_richtiger_Version_in_Eigenschaften.dll"
+#Setze installierte Version auf 0
+$installedVer = 0
+#Setze Bool für Installation
+$gehtWeiter = 0
+#Baue kompletten Pfad zur Datei
+$64bitPath = ${env:ProgramFiles} + $dateiname
+$32bitPath = ${env:ProgramFiles(x86)} + $dateiname
+
+Write-Host ("Installation von " + $progName)
+
+#Überprüfe auf aktuell installierte Version und vergleiche sie mit der Gewünschten.
+
+#Überprüfe, ob der Pfad im 64-Bit-Programmverzeichnis verfügbar ist
+if (Test-Path $64bitPath) {
+    #Wenn der 64-Bit-Pfad gefunden wurde, überprüfe Dateiversion.
+    $installedVer = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($64bitPath).FileVersion
+    if ($installedVer -lt $desiredProgVer) {
+        #Wenn sie kleiner als die Gewünschte ist, geht es weiter.
+            $gehtWeiter = 1
+    }
+    else {
+        #Sonst beende diese Schleife.
+    }
+}
+else {
+    #Wenn der 64-Bit-Pfad nicht gefunden wurde, überprüfe, ob der Pfad im 32-Bit-Programmverzeichnis verfügbar ist.
+    if (Test-Path $32bitPath) {
+        #Wenn der 32-Bit-Pfad gefunden wurde, überprüfe Dateiversion.
+        $installedVer = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($32bitPath).FileVersion
+        if ($installedVer -lt $desiredProgVer) {
+                #Wenn sie kleiner als die Gewünschte ist, geht es weiter.
+                $gehtWeiter = 1
+            }
+        else {
+            #Sonst beende diese Schleife.
+        }
+    }
+    else {
+        #Wenn weder der 64-, noch der 32-Bit-Pfad gefunden wurden, installiere frisch.
+        $gehtWeiter = 1
+        }
+}
+
+if ($gehtWeiter) {
+    #Wenn keine gewünschte Version gefunden wurde, installiere.
+    Write-Host ("Starte Installation von " + $progName + ".")
+    Set-Location "\\server\software\Supertolles Programm"
+    & .\Programminstaller_17_0.exe /S
+    Write-Host ("Skript beendet.")
+}
+else {
+    #Sonst beende.
+    Write-Host ("Gewünschte Version von " + $progName + " ist bereits vorhanden. Beende das Skript.")
+}
